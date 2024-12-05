2e2795c5ed81e5c0768e55f7b7a48e863c4bf6bd
mirziyo <mirziyo>
2024-12-06 00:53:06 +0500

    myteam

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const headerToggleBtn = document.querySelector(".header__toggle-btn");
+const headerInnerWrapper = document.querySelector(".header__inner-wrapper");
+const headerInnerWrapperBtn = document.querySelector(
+  ".header__inner-wrapper-btn"
+);
+
+headerToggleBtn.addEventListener("click", () => {
+  headerInnerWrapper.classList.add("show-nav");
+});
+
+headerInnerWrapperBtn.addEventListener("click", () => {
+  headerInnerWrapper.classList.remove("show-nav");
+});
